2ceb2843c83af93b6b1b29306494067b49fe9fdc
josefin-ferrada <47111210+josefin-ferrada@users.noreply.github.com>
2019-03-19 11:53:57 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+window.data = {
+
+    filterData: (pokemonData, condition) => {
+        let result;
+        if (condition === "none") {
+            result = pokemonData;
+        } else {
+            result = pokemonData.filter(element => {
+                if (element.type.includes(condition)) {
+                    return element;
+                }
+            });
+        }
+
+        return result;
+
+    }
+}
