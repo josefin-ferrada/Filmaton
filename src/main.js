24c0a3a264aa4687dc6bede8704142f4c703ae28
josefin-ferrada <josefin.ferrada95@gmail.com>
2019-03-19 21:41:14 -0300

    mostrar resultados de la consulta a la api con fecth

--- src/main.js
+++ src/main.js
@@ -1,21 +1,53 @@
 
 
+let btn = document.getElementById('search');
 
-function getTitle(){
-  let title =document.getElementById('title').value;
-  
+btn.addEventListener('click', () => {
+
+let year = document.getElementById('year').value;
+let title =document.getElementById('title').value;
+let type = document.getElementById('type').value;
+let url= 'http://www.omdbapi.com/?s='+title+'&y='+year+'&apiKey=5648970a';
+if (type != '') {
+	url += '&type='+type;
 
 }
-function getYear(){
-  let year = documnet.getElementById('year')
-}
+console.log(url)
 
-let url = 'http://www.omdbapi.com/?s=twilight&apikey=5648970a'
 
 fetch(url)
 
 .then(response => response.json())
+
+
 .then(data => {
-  console.log(data)
+	let cardContainer = document.getElementById("id1");
+	let text ='';
+
+	data.Search.forEach(function(element){
+	 	text += `<div class="row">
+	    <div class="col s12 m6">
+	      <div class="card">
+	        <div class="card-image">
+	          <img src="${element.Poster}">
+	          <span class="card-title">Card Title</span>
+	          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+	        </div>
+	        <div class="card-content">
+	          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+	        </div>
+	      </div>
+	    </div>
+	  </div>`;
+
+	 });
+
+  	cardContainer.innerHTML= text;
+
+ 
+
 })
-.catch(err=>console.log(err))
+.catch(err => console.log(err));
+})
+
+
