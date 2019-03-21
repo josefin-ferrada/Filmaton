a4309b29b0d66a0c34b57b19591094bbe449aa3b
josefin-ferrada <josefin.ferrada95@gmail.com>
2019-03-21 01:12:24 -0300

    mostrando género, director, en modal, y en rows con cards

--- src/main.js
+++ src/main.js
@@ -22,36 +22,116 @@ fetch(url)
 
 
 .then(data => {
-	let cardContainer = document.getElementById("id1");
-	let text ='';
-
-	data.Search.forEach(function(element){
-		 text += `<div class="row">
-		 <div class="col s12 m6">
-	      <div class="card">
-	        <div class="card-image">
-	          <img src="${element.Poster}">
-	          
-	          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-	        </div>
-	        <div class="card-content">
-
-						<p><span class="card-title">${element.Title}</span>.</p>
-						
-						<p>${element.Year}</p>
+	if (data.Response == "True"){
+		let cardContainer = document.getElementById("id1");
+		let text ='';
+		console.log(data);
+		data.Search.forEach(function(element){
+			 text += `
 
 
-	        </div>
-	      </div>
-	    </div>
-		</div>`
-		
+			
+			
+				<div class="col l3" >
+			      <div class="card large" class="card-title">
+			        <div class="card-image">
+			          <img src="${element.Poster}">
+			          
+			        </div>
+			        <div class="card-content">
 
-	 });
+								<p><span class="card-title">${element.Title}</span>.</p>
+								<a class="waves-effect waves-light btn modal-trigger" id="${element.imdbID}"href="#modal-${element.imdbID}">Ver más</a>
+								<p>${element.Year}</p>
+			        </div>
+			      </div>
+		    	</div>
 
-  	cardContainer.innerHTML= text;
+			
+			<div id="modal-${element.imdbID}" class="modal">
+			    <div class="modal-content">
+			      	
+			    
+				    <div class="row"> 
 
- 
+					    <div class="col s12 s4 m4 l4 ">
+							<div class="align-image">
+					      	<img src="${element.Poster}">
+					      	</div>
+					   	</div>
+
+				    	<div class="class="col s4 m4 l4" id="modal-body${element.imdbID}">
+				    	</div> 
+
+				    </div>
+
+				    <div class="modal-footer">
+				      	<a href="#!" class="waves-effect waves-light btn modal-close ">Cerrar</a>
+				    </div>
+			    </div>
+			</div>`;
+			
+
+		});
+
+	  	cardContainer.innerHTML= text;
+	  	var elems = document.querySelectorAll('.modal');
+	    var instances = M.Modal.init(elems);
+
+	    let btnModals = document.querySelectorAll('.modal-trigger');
+
+	   	btnModals.forEach(function(element){
+	   		element.addEventListener('click', () => {
+	   			console.log(element.id);
+	   			fetch('http://www.omdbapi.com/?i='+element.id+'&apiKey=5648970a')
+
+	   			.then(response => response.json())
+
+	   			.then(respuesta => {
+		   			console.log(respuesta);
+		   			let modalNew= document.getElementById('modal-body'+element.id);
+		   			modalNew.innerHTML = `
+		   								 <table class="responsive-table">
+									        <thead>
+									          <tr>
+									          	<th>Título:</th>
+									            <td>${respuesta.Title}</td>
+									          
+									              
+									          </tr>
+									          <tr>
+									            <th>Año:</th>
+									            <td>${respuesta.Year}</td>
+									              
+									          </tr>
+									          <tr>
+									            <th>Director:</th>
+									            <td>${respuesta.Director}</td>
+									              
+									          </tr>
+									          <tr>
+									            <th>Reparto:</th>
+									            <td>${respuesta.Actors}</td>
+									              
+									          </tr>
+									        </thead>
+
+									        <tr>
+									            <th>Género:</th>
+									            <td>${respuesta.Genre}</td>
+									              
+									          </tr>
+										</table>`;
+	   			})
+	   		});
+	    });
+	}
+
+	else{
+
+		alert("Llena el campo de titulo")
+	}
+
 
 })
 .catch(err => console.log(err));
