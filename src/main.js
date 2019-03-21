1e936e906ea3cce2bb49f5e6d211f72971e9dcdb
TatianaCastroLizama <tcl.contacto@gmail.com>
2019-03-21 12:08:58 -0300

    cambios en index.html

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-
+const carrusel = document.getElementsByClassName(carousel);
 
 let btn = document.getElementById('search');
 
@@ -56,4 +56,41 @@ fetch(url)
 })
 .catch(err => console.log(err));
 })
+
+
+//	let title = document.getElementById('title').value;
+//		let urlimdb= 'http://www.omdbapi.com/?s='+title+'&i='+imdbID+'&apiKey=3067dff4';
+//if (type != '') {
+//	url += '&type='+type;
+
+
+
+//		fetch(urlimdb)
+
+//		.then(response => response.json())
+		
+		
+//		.then(data => {
+//			let cardContainer = document.getElementById("id1");
+//			let text ='';
+		
+//			data.Search.forEach(function(element){
+//				 text += `<div class="row">
+//					<div class="col s12 m6">
+//						<div class="card">
+//							<div class="card-image">
+//								<img src="${element.Director}">
+//								<span class="card-title">Card Title</span>
+//								<a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+//							</div>
+//							<div class="card-content">
+//								<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+//							</div>
+//						</div>
+//					</div>
+//				</div>`;
+		
+//			 });
+
+//			})
 
